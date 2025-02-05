7610976b8294d5b3c26730cadf3cfa5155fff687
MaximeB <maxounu@gmail.com>
2025-02-05 09:21:24 +0100

    refactor: remove commented French descriptions in DutyRoutes

--- routes/DutyRoutes.js
+++ routes/DutyRoutes.js
@@ -7,16 +7,16 @@ const {
   deleteDuty,
 } = require("../controllers/DutyController");
 
-// Récupérer le planning des gardes
+
 router.get("/", getDuties);
 
-// Ajouter une nouvelle garde
+
 router.post("/", addDuty);
 
-// Modifier une garde existante
+
 router.put("/:id", updateDuty);
 
-// Supprimer une garde existante
+
 router.delete("/:id", deleteDuty);
 
 module.exports = router;
