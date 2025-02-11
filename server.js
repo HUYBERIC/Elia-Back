616c2e9f0b5b0b0e202f9a0f03311390a7f4a360
huyberic <huyberic@gmail.com>
2025-02-11 13:47:29 +0100

    chore: remove unused CORS middleware and delete vercel.json configuration

--- server.js
+++ server.js
@@ -37,13 +37,13 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"]
 };
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
-});
+}); */
 
 app.use(cors(corsOptions));
 
