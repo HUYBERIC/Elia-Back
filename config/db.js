7c6ded1df3b361a426779905299e37bc9faadbd8
huyberic <huyberic@gmail.com>
2025-02-14 16:02:56 +0100

    feat: update MongoDB connection to use environment variable for URI

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/eliadb?retryWrites=true&w=majority&appName=EliaDB`;
-
-console.log("🔍 Connecting to MongoDB:", MONGO_URI);
+console.log("🔍 Connecting to MongoDB:", process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{userMongoClient: true});
     console.log("✅ Connected to MongoDB");
   } catch (err) {
     console.error("❌ MongoDB Connection Error:", err);
