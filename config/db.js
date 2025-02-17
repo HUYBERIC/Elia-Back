7f9e8a931b62b13ffcf638dcc454139e6a2ebe4b
huyberic <huyberic@gmail.com>
2025-02-17 12:09:53 +0100

    feat: add CapRover configuration and Docker setup for deployment

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-console.log("🔍 Connecting to MongoDB:", process.env.MONGO_URI);
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/eliadb?retryWrites=true&w=majority&appName=EliaDB`;
+
+console.log("🔍 Connecting to MongoDB:", MONGO_URI);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{userMongoClient: true});
+    await mongoose.connect(MONGO_URI);
     console.log("✅ Connected to MongoDB");
   } catch (err) {
     console.error("❌ MongoDB Connection Error:", err);
@@ -14,4 +16,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
